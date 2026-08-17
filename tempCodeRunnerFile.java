a6c4a55d78ac5e38c55328fbd829f3315b666c06
Shivam Mishra <shivammishra10m@gmail.com>
2026-08-17 18:59:15 +0530

    Deleting tempRunnerCode.java File

--- tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-                do {
-                    System.out.println("\nWhat You want to do?");
-                    System.out.println("\n1.View Bank Details\n2.View User Details\n3.View Account Details\n4.Check Minimum Balance\n5.Exit");
-
-                    System.out.print("\nEnter your choice: ");
-                    userChoice = scan.nextInt();
-
-                } while (userChoice > 5 || userChoice < 1);
